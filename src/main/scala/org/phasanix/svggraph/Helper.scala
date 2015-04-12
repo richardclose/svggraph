5ef9eb5513b7d49d12dcf9d00aa2214019c45675
Richard Close <richard@phasanix.org>
2015-04-12 23:22:02 +0100

    Introduced Helper.StringBuilderOps

--- src/main/scala/org/phasanix/svggraph/Helper.scala
+++ src/main/scala/org/phasanix/svggraph/Helper.scala
@@ -3,7 +3,60 @@ package org.phasanix.svggraph
 /**
  * helper methods
  */
-private[svggraph] object Helper {
+object Helper {
 
+  /**
+   * Fast append double value to string buffer with fixed dp
+   * Thread-safe alternative to java.text.NumberFormat
+   */
+  def append(sb: StringBuilder, value: Double, dp: Int): Unit = {
+    val insertAt = sb.length
+
+    val absV = if (value < 0) {
+      sb.append('-')
+      -value
+    } else value
+
+    var intV = absV.toInt
+
+    while (intV > 0) {
+      sb.insert(insertAt, ((intV % 10) + '0').toChar)
+      intV /= 10
+    }
+
+    if (dp > 0) {
+      val pow = dp match {
+        case 1 => 10
+        case 2 => 100
+        case 3 => 1000
+        case 4 => 10000
+        case _ =>
+          var count = dp
+          var p = 1
+          while (count > 0) {
+            p *= 10
+            count -= 1
+          }
+          p
+      }
+
+      var decV = ((absV % 1) * pow).toInt
+      sb.append('.')
+      val insertDp = sb.length
+      var count = dp
+      while (count > 0) {
+        sb.insert(insertDp, ((decV % 10) + '0').toChar)
+        decV /= 10
+        count -= 1
+      }
+    }
+  }
+
+  implicit class StringBuilderOps(val sb: StringBuilder) extends AnyVal {
+    def fmt(value: Double, dp: Int): StringBuilder = {
+      append(sb, value, dp)
+      sb
+    }
+  }
 
 }
---
+++ src/test/scala/HelperTest.scala
@@ -0,0 +1,64 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+import org.phasanix.svggraph.Helper._
+
+/**
+ *
+ */
+class HelperTest extends FlatSpec with Matchers {
+
+  "Helper formatting" should "correctly encode float" in {
+    val sb = new StringBuilder()
+    sb.append("[").fmt(123.45, 3).append("]")
+    sb.toString shouldEqual "[123.450]"
+  }
+
+  it should "be faster than NumberFormat" in {
+
+    val sb = new StringBuilder()
+    val numFmt = java.text.NumberFormat.getInstance()
+    numFmt.setMinimumFractionDigits(3)
+    numFmt.setMaximumFractionDigits(3)
+    numFmt.setGroupingUsed(false)
+
+    val count = 500000
+    var i: Int = 0
+    var start: Long = 0L
+
+    // warm up loop for Helper and NumberFormat
+    i = 10000
+    while (i > 0) {
+      sb.append(numFmt.format(123.45))
+      sb.fmt(123.45, 3)
+      sb.setLength(0)
+      i -= 1
+    }
+
+    // Measure Helper.fmt
+    i = count
+    start = System.nanoTime()
+    while (i > 0) {
+      sb.fmt(123.45, 3)
+      sb.setLength(0)
+      i -=  1
+    }
+
+    val t1 = (System.nanoTime().toDouble - start)/count
+
+    // Measure NumberFormat
+    i = count
+    start = System.nanoTime()
+    while (i > 0) {
+      sb.append(numFmt.format(123.45))
+      sb.setLength(0)
+      i -= 1
+    }
+
+    val t2 = (System.nanoTime().toDouble - start)/count
+
+    println(s"call to Helper.append(): $t1 ns call to NumberFormat.format(): $t2")
+
+    (t2 - t1 > 0) shouldBe true
+  }
+
+}
